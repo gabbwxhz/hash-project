6beada75fc79914fec3f9b756c0a2d4fde1d056f
gabriel <vnvsgabe@gmail.com>
2023-08-05 13:35:16 -0300

    criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
+import Icon from '../icon/Icon'
 
 import styles from './Header.module.css'
 
@@ -8,6 +9,9 @@ function Header() {
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
             <Subtitle>Criado por Gabriel Picoli</Subtitle>
+            <div className={styles.iconContent}>
+                <Icon iconName='github' link='https://github.com/gabbwxhz' />
+            </div>
         </div>
     )
 }
